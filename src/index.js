b1d503cfa531e2c0be43da060265f0b7f28ffbbb
Manuel Sena <32990936+ManuelSena@users.noreply.github.com>
2020-01-09 12:43:49 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AppRouter } from "./router";
+import * as serviceWorker from "./build/serviceWorker";
 ReactDOM.render(React.createElement(AppRouter, null), document.getElementById("root"));
 serviceWorker.register()
 //# sourceMappingURL=index.js.map
