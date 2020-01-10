2688497c0d2f8d903ef2055552bfd21fa3e05b06
Manuel Sena <32990936+ManuelSena@users.noreply.github.com>
2020-01-09 17:40:29 -0800

    Update index.js
    import '../node_modules/bootstrap/dist/css/bootstrap.min.css';

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AppRouter } from "./router";
 import * as serviceWorker from "./build/serviceWorker";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(React.createElement(AppRouter, null), document.getElementById("root"));
 serviceWorker.unregister()
 //# sourceMappingURL=index.js.map
