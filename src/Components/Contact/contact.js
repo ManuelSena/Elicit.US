11764747c47b983e0e528a9a71d008a5ffb0ad7a
ManuelS <manuelsena@me.com>
2020-01-13 20:50:04 -0800

    Revert "yolo"
    This reverts commit 78383ae8f1e939a3e14fa4bac560ff022b87dca6.

---
+++ src/Components/Contact/contact.js
@@ -0,0 +1,5 @@
+import * as React from "react";
+export const ContactPage = () => {
+    return (React.createElement("div", null, "This is Contact Us page."));
+};
+//# sourceMappingURL=contact.js.map
