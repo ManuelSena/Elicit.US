2241a738b855f2ea1ac8d74938d3571e12e062fa
ManuelS <manuelsena@me.com>
2020-01-15 21:26:49 -0800

    brutal background

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,5 +1,5 @@
 import * as React from "react";
 export const HomePage = () => {
-    return (React.createElement("div", { style: { color: "#f8f8ff", width: "100%", float: "left", padding: "30px 0", overflow: "hidden", backgroundColor: "#000000" } }, "This is the elicit.US Home page."));
+    return (React.createElement("div", { style: { color: "#f8f8ff", width: "100%", float: "left", padding: "30px 0", overflow: "hidden", backgroundColor: "#000000" } }, "elicit.US coming soon."));
 };
 //# sourceMappingURL=home.js.map
--- src/Components/Home/home.tsx
+++ src/Components/Home/home.tsx
@@ -3,7 +3,7 @@
 export const HomePage: React.StatelessComponent<{}> = () => {
     return (
         <div style={{ color:"#f8f8ff", width: "100%", float: "left", padding: "30px 0", overflow: "hidden", backgroundColor: "#000000" }}>
-                This is the elicit.US Home page.
+               elicit.US coming soon.
         </div>
     );
 }
