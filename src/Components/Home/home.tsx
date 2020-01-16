e01c003f365fcaa57496d03831e7e8e7706bca51
ManuelS <manuelsena@me.com>
2020-01-15 21:46:59 -0800

    centereded

--- src/Components/Home/home.tsx
+++ src/Components/Home/home.tsx
@@ -2,7 +2,7 @@
 
 export const HomePage: React.StatelessComponent<{}> = () => {
     return (
-        <div style={{ color:"#f8f8ff", width: "100%", float: "left", padding: "30px 0", overflow: "hidden", backgroundColor: "#000000" }}>
+        <div style={{ textAlign:"center", color:"#f8f8ff", width: "100%", float: "left", padding: "30px 0", overflow: "hidden", backgroundColor: "#000000" }}>
                elicit.US coming soon.
         </div>
     );
