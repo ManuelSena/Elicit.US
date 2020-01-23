cbd638a9f46ffedba9001ab332d777a775b7a644
ManuelS <manuelsena@me.com>
2020-01-23 09:00:49 -0800

    caps

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,5 +1,5 @@
 import * as React from "react";
 export const HomePage = () => {
-    return (React.createElement("div", { style: { textAlign: "center", color: "#f8f8ff", width: "100%", padding: "30px 0", backgroundColor: "#000000" } }, "Elicit is dedicated to the creation & maintaining of community connections between groups and individuals whose lives are enmeshed in the music, art, and entertainment industry. Our priority is to facilitate the support of bands, groups, and people involved in order to spread their creative work and personal message.  We work for the Aspiring and Inspiring."));
+    return (React.createElement("div", { style: { textAlign: "center", color: "#f8f8ff", width: "100%", padding: "30px 30px", backgroundColor: "#000000" } }, "Elicit is dedicated to the creation & maintaining of community connections between groups and individuals whose lives are enmeshed in the Music, Art, and Entertainment industry. Our priority is to facilitate the support of bands, groups, and people involved in order to spread their creative work and personal message.     We work for the Aspiring and Inspiring."));
 };
 //# sourceMappingURL=home.js.map
--- src/Components/Home/home.tsx
+++ src/Components/Home/home.tsx
@@ -2,8 +2,8 @@
 
 export const HomePage: React.StatelessComponent<{}> = () => {
     return (
-        <div style={{ textAlign:"center", color:"#f8f8ff", width: "100%", padding: "30px 0", backgroundColor: "#000000" }}>
-               Elicit is dedicated to the creation & maintaining of community connections between groups and individuals whose lives are enmeshed in the music, art, and entertainment industry. Our priority is to facilitate the support of bands, groups, and people involved in order to spread their creative work and personal message.  We work for the Aspiring and Inspiring.
+        <div style={{ textAlign:"center", color:"#f8f8ff", width: "100%", padding: "30px 30px", backgroundColor: "#000000" }}>
+               Elicit is dedicated to the creation & maintaining of community connections between groups and individuals whose lives are enmeshed in the Music, Art, and Entertainment industry. Our priority is to facilitate the support of bands, groups, and people involved in order to spread their creative work and personal message.     We work for the Aspiring and Inspiring.
     
         </div>
  
