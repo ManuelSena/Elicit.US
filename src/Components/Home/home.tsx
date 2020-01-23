9d5cfd271ae5237b2ac15a0d7870287122c3e938
Manuel Sena <32990936+ManuelSena@users.noreply.github.com>
2020-01-22 17:41:51 -0800

    Update home.tsx

--- src/Components/Home/home.tsx
+++ src/Components/Home/home.tsx
@@ -3,7 +3,7 @@
 export const HomePage: React.StatelessComponent<{}> = () => {
     return (
         <div style={{ textAlign:"center", color:"#f8f8ff", width: "100%", float: "left", padding: "30px 0", overflow: "hidden", backgroundColor: "#000000" }}>
-               elicit.US coming soon.
+               Elicit is dedicated to the creation & maintaining of community connections between groups and individuals whose lives are enmeshed in the music, art, and entertainment industry. With this podcast, our priority is to facilitate the support of bands, groups, and people involved in order to spread their creative work and personal message. We work for the Aspiring and Inspiring.
         </div>
     );
 }
