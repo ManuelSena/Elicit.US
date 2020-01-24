f88f38025f3befbf47d7b1d0c44b9e01f569dd97
ManuelS <manuelsena@me.com>
2020-01-23 19:10:44 -0800

    minor

--- src/Components/Home/home.tsx
+++ src/Components/Home/home.tsx
@@ -3,7 +3,7 @@
 export const HomePage: React.StatelessComponent<{}> = () => {
     return (
         <div style={{ textAlign:"center", color:"#f8f8ff", width: "100%", padding: "30px 30px", backgroundColor: "#000000" }}>
-               Elicit is dedicated to the creation & maintaining of community connections between groups and individuals whose lives are enmeshed in the Music, Art, and Entertainment industry. Our priority is to facilitate the support of bands, groups, and people involved in order to spread their creative work and personal message.     We work for the Aspiring and Inspiring.
+               Elicit is dedicated to the creation & maintaining of community connections between groups and individuals whose lives are enmeshed in the Music, Art, and Entertainment Industry. Our priority is to facilitate the support of bands, groups, and people involved in order to spread their creative work and personal message.     We work for the Aspiring and Inspiring.
     
         </div>
  
